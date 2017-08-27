c98fd09e8dc96a4918439f6f6553c0e4d67697ff
hrodberaht <robpszczolka@gmail.com>
2017-08-27 20:53:38 +0200

    add domain model and repository

---
+++ src/Models/FakeProductRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace src.Models
+{
+    public class FakeProductRepositiory : IProductRepositiory
+    {
+        public IEnumerable<Product> Products => new List<Product>{
+            new Product {Name = "Ball", Price = 21 },
+            new Product {Name = "Toy", Price = 27 },
+            new Product {Name = "Car", Price = 34 }
+
+        };
+    }
+}
---
+++ src/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace src.Models
+{
+    public class Product
+    {
+        public int ProductID {get; set;}
+        public string Name  {get; set;}
+
+        public decimal Price { get; set; }
+    }
+}
